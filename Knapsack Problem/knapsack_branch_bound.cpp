6fa1e7c5432f8e3354c66fb6f739a9a6c9d2e120
fer <ferconde87@gmail.com>
2018-09-22 18:04:21 -0300

    Knapsack Problem: Algoritmo Branch & Bound como la gente. Mejoras en la estimación para podar y en el brancheo.

--- Knapsack Problem/knapsack_branch_bound.cpp
+++ Knapsack Problem/knapsack_branch_bound.cpp
@@ -18,14 +18,11 @@ int n = 0;
 //estimation of the current solution considering the current value of the knapsack + the best items
 //in terms of value/weight outside the knapsack using linear relaxation
 double estimate(int current_value, int capacity, int start){
-    // cout << "-------------- estimate --------------------------" << endl;
-    // cout << current_value << " " << capacity << " " << start << endl;
     int room = capacity;
     double estimation = current_value;//value of the items already in the knapsack
     int n = order.size();
-    for(int i = 1; i <= n; i++){
+    for(int i = start; i <= n; i++){
         int index = order[i];
-        if(index < start || current_solution[index]) continue;
         int weight = weights[index];
         int value  = values[index];
         portions[index] = 1;
@@ -39,67 +36,43 @@ double estimate(int current_value, int capacity, int start){
             break;
         }
     }
-    // cout << "-------------- END estimate --------------------------" << endl;
-    return estimation;//(int)ans;
+    return estimation;
 }
 
-
-void print_current_solution(vector<int> & vs){
-    for(int i = 1; i <= n; i++){
-        cout << vs[i] << " ";
-    }
-    cout << endl;
-}
-
-void print_used(){
-    for(int i = 1; i < used.size(); ++i) cout << used[i] << " ";
-    cout << endl;
-}
 
 void knapsack(int current_value, int remaining_capacity, double estimation, int index, vector<int> & current_solution){
-    // cout << current_value << " " << remaining_capacity << " " << estimation << " " << index << " # "; 
-    // print_current_solution(current_solution);
+    
     if(estimation <= best_value) return;//bound
-
 
     if(index == n+1){
         if(current_value > best_value){
             best_value = current_value;
-            // cn(remaining_capacity)
-            // cn(current_value)
-            // cout << "[ " << remaining_capacity << " | " << current_value << " ] => ";
             for(int i = 1; i <= n; i++){
                 best_solution[i] = current_solution[i];
-                // cout << best_solution[i] << " ";
             }
-            // cout << endl;
         }
         return;
     }
 
-    if(weights[index] <= remaining_capacity){
-        // cn(values[index])
-        // cn(weights[index])
-        double new_estimation = estimate(current_value+values[index], remaining_capacity-weights[index], index+1);
-        current_solution[index] = 1;
-        knapsack(current_value+values[index], remaining_capacity-weights[index], new_estimation, index+1, current_solution);
-        //si portions[indx] == 1 no hay que calcular la nueva estimation, ya que el item ya es parte de ella
-    }
-    if(current_solution[index]){
-        current_solution[index] = 0;
-    }
-    double new_estimation = estimate(current_value, remaining_capacity, index+1);
-    // cn(new_estimation)
-    knapsack(current_value, remaining_capacity, new_estimation, index+1, current_solution);
+    while(index <= n && weights[order[index]] > remaining_capacity) index++;
+    
+    if(index <= n ){ // => weights[index] <= remaining_capacity
+        double new_estimation = estimate(current_value+values[order[index]], remaining_capacity-weights[order[index]], index+1);
+        current_solution[order[index]] = 1;
+        knapsack(current_value+values[order[index]], remaining_capacity-weights[order[index]], new_estimation, index+1, current_solution);
+        current_solution[order[index]] = 0;
+        new_estimation = estimate(current_value, remaining_capacity, index+1);
+        knapsack(current_value, remaining_capacity, estimation, index+1, current_solution);
+    }else{
+        knapsack(current_value, remaining_capacity, estimation, index, current_solution);
+    } 
 }
 
 
 void solve_branch_bound(string filename){
     ifstream fin(filename);
-    // int n;
     int capacity;
     fin >> n >> capacity;
-    
     values.resize(n+1);
     weights.resize(n+1);
     order.resize(n+1);
@@ -124,7 +97,7 @@ void solve_branch_bound(string filename){
 
     knapsack(0, capacity, estimation, 1, current_solution);
     
-    cout << best_value << " " << 0 << endl;
+    cout << best_value << " " << 1 << endl;
     for(int i = 1; i <= n; i++) cout << best_solution[i] << " ";
     cout << endl;
     cout.flush();
@@ -135,6 +108,5 @@ int main(int argc, char** argv){
     if(argc < 2) return 0;
     string filename = argv[1];
     solve_branch_bound(filename);
-    
     return 0;
 }
